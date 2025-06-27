092178705f69c72546186866b1b653a55f63e8e7
rohangahlot27 <rohan.gahlot27@gmail.com>
2025-06-27 09:46:35 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['lucide-react'], // Add any library here causing warnings
+      onwarn(warning, warn) {
+        // Skip known safe warning
+        if (
+          warning.message &&
+          warning.message.includes('externalized for browser compatibility')
+        ) return;
+        warn(warning);
+      },
     },
   },
-})
+});
 
